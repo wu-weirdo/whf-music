d71aa39e821524dbacea6414b41660c1fa5b3bb0
weirdo_wu <982280485@qq.com>
2023-04-23 16:44:42 +0800

    整合spring security

--- music-server/src/main/java/com/example/yin/config/WebMvcConfig.java
+++ music-server/src/main/java/com/example/yin/config/WebMvcConfig.java
@@ -1,17 +1,8 @@
 package com.example.yin.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 /**
  * @Author 祝英台炸油条
