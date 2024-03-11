48b36c3e1551abe46ae0112572bec9c37abec27f
whf <982280485@qq.com>
2024-03-11 22:25:28 +0800

    增加超时时间

--- music-client/src/api/request.ts
+++ music-client/src/api/request.ts
@@ -4,7 +4,7 @@ import store from "@/store";
 
 const BASE_URL = process.env.NODE_HOST;
 
-axios.defaults.timeout = 5000; // 超时时间设置
+axios.defaults.timeout = 60000; // 超时时间设置
 axios.defaults.withCredentials = true; // true允许跨域
 axios.defaults.baseURL = BASE_URL;
 // Content-Type 响应头
